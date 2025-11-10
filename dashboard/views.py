16200b4ba7fa3cb6163f50225fa29c54ce5faf64
muaid773 <muaidali773@gmail.com>
2025-11-10 17:39:37 +0300

    MVP veirson 1.1.6 som changes in teachers veiw and templet

--- dashboard/views.py
+++ dashboard/views.py
@@ -3,7 +3,9 @@ from core.models import Student, Subject, Teacher
 # Create your views here.
 def main_dashboard(request):
     return render(request, 'dashboard_page.html',
-                  {'teachers':len(Teacher.object.all()),
-                   'students':len(Student.object.all()),
-                   'subjects':len(Subject.object.all())
+                  {'teachers_len':Teacher.objects.all().count(),
+                   'teachers_len_active':Teacher.objects.all().filter(is_active=True).count(),
+                   'students_len':Student.objects.all().count(),
+                   'students_len_active':Student.objects.all().filter(active=True).count(),
+                   'subjects_len':Subject.objects.all().count(),
                    })
